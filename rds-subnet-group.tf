7a881e36fab697ad6fc178b3a1741b7f38b3bb98
marchhare149 <buithiphuongthao14091994@gmail.com>
2026-02-06 17:39:00 +0100

    fix db subnet group subnet names

--- rds-subnet-group.tf
+++ rds-subnet-group.tf
@@ -2,8 +2,8 @@ resource "aws_db_subnet_group" "db_subnet_group" {
   name       = "db-subnet-group"
   description = "DB subnet group for RDS"
   subnet_ids = [
-    aws_subnet.private_subnet_a.id,
-    aws_subnet.private_subnet_b.id
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
   ]
 
   tags = {
