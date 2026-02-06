fdad19671228402645bc566b17961c5d03f86a01
marchhare149 <buithiphuongthao14091994@gmail.com>
2026-02-06 16:03:27 +0100

    remove duplicate ingress block

--- sg.tf
+++ sg.tf
@@ -96,9 +96,29 @@ resource "aws_security_group" "rds_sg" {
 
   tags = { Name = "rds-sg" }
 }
-ingress {
-  from_port       = 22
-  to_port         = 22
-  protocol        = "tcp"
-  security_groups = [aws_security_group.bastion_sg.id]
+resource "aws_security_group" "wp_sg" {
+  name   = "wordpress-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
