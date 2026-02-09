77ce4aefad850306b29d056f992bf9c980c318e8
marchhare149 <buithiphuongthao14091994@gmail.com>
2026-02-09 17:51:35 +0100

    Fix WordPress document root in user data

--- userdata.sh
+++ userdata.sh
@@ -9,14 +9,18 @@ systemctl enable httpd
 cd /var/www/html
 wget https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz
-chown -R apache:apache wordpress
-chmod -R 755 wordpress
 
-cp wordpress/wp-config-sample.php wordpress/wp-config.php
+cp -r wordpress/* /var/www/html/
+rm -rf wordpress latest.tar.gz
 
-sed -i "s/database_name_here/wordpress/" wordpress/wp-config.php
-sed -i "s/username_here/wpuser/" wordpress/wp-config.php
-sed -i "s/password_here/wpPassword123!/" wordpress/wp-config.php
-sed -i "s/localhost/wordpress-db.chy6uimi0gsm.us-west-2.rds.amazonaws.com/" wordpress/wp-config.php
+chown -R apache:apache /var/www/html
+chmod -R 755 /var/www/html
+
+cp wp-config-sample.php wp-config.php
+
+sed -i "s/database_name_here/wordpress/" wp-config.php
+sed -i "s/username_here/wpuser/" wp-config.php
+sed -i "s/password_here/wpPassword123!/" wp-config.php
+sed -i "s/localhost/wordpress-db.chy6uimi0gsm.us-west-2.rds.amazonaws.com/" wp-config.php
 
 systemctl restart httpd
