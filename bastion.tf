8f4708cc473ab8648bd856be832af1eb3de32f36
marchhare149 <buithiphuongthao14091994@gmail.com>
2026-02-09 15:35:52 +0100

    Fix invalid AMI for bastion

--- bastion.tf
+++ bastion.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "bastion" {
-  ami                    = "ami-0d593311db5abb..."
+  ami                    = "ami-0b8c6b923777519db"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_a.id
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
