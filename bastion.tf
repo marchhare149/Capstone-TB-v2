fca8eed97aabfb07970e846aa65144ea5d34e463
marchhare149 <buithiphuongthao14091994@gmail.com>
2026-02-09 15:32:29 +0100

    Add key_name to bastion instance

--- bastion.tf
+++ bastion.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "bastion" {
-  ami                    = "ami-0d593311db5abb72b" # Amazon Linux 2 (us-west-2)
+  ami                    = "ami-0d593311db5abb..."
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_a.id
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
+  key_name = var.key_name   
 
   tags = {
     Name = "bastion-host"
