045336efef0e8bb58f68f732d007f7479fa1e50b
marchhare149 <buithiphuongthao14091994@gmail.com>
2026-02-25 16:16:37 +0100

    variables 3

--- ses.tf
+++ ses.tf
@@ -1,17 +1,4 @@
 
-resource "aws_ses_domain_identity" "domain" {
-  domain = var.ses_domain
-}
-
-resource "aws_ses_domain_dkim" "dkim" {
-  domain = aws_ses_domain_identity.domain.domain
-}
-
-resource "aws_ses_domain_mail_from" "mail_from" {
-  domain           = aws_ses_domain_identity.domain.domain
-  mail_from_domain = "${var.ses_mail_from_subdomain}.${var.ses_domain}"
-}
-
 output "ses_verification_token" {
   value = aws_ses_domain_identity.domain.verification_token
 }
@@ -22,4 +9,11 @@ output "ses_dkim_tokens" {
 
 output "ses_mail_from_domain" {
   value = aws_ses_domain_mail_from.mail_from.mail_from_domain
+}
+variable "ses_from_email" {
+  type = string
+}
+
+resource "aws_ses_email_identity" "from" {
+  email = var.ses_from_email
 }
