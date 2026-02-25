8e6b74c93b6fc939e8b8fbc4c2253eab83af58dc
marchhare149 <buithiphuongthao14091994@gmail.com>
2026-02-25 15:02:24 +0100

    some new tf files

---
+++ cloudwatch.tf
@@ -0,0 +1,47 @@
+variable "alarm_email" {
+  type = string
+}
+
+resource "aws_sns_topic" "alerts" {
+  name = "${var.project_name}-alerts"
+}
+
+resource "aws_sns_topic_subscription" "alerts_email" {
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = var.alarm_email
+}
+
+resource "aws_cloudwatch_metric_alarm" "asg_cpu_high" {
+  alarm_name          = "${var.project_name}-asg-cpu-high"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  period              = 300
+  threshold           = 70
+  statistic           = "Average"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.wordpress.name
+  }
+
+  alarm_actions = [aws_sns_topic.alerts.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "rds_free_storage_low" {
+  alarm_name          = "${var.project_name}-rds-free-storage-low"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  period              = 300
+  threshold           = 2147483648
+  statistic           = "Average"
+  metric_name         = "FreeStorageSpace"
+  namespace           = "AWS/RDS"
+
+  dimensions = {
+    DBInstanceIdentifier = aws_db_instance.wordpress.id
+  }
+
+  alarm_actions = [aws_sns_topic.alerts.arn]
+}
---
+++ waf.tf
@@ -0,0 +1,63 @@
+resource "aws_wafv2_web_acl" "cdn" {
+  provider = aws.use1
+  name     = "${var.project_name}-cf-waf"
+  scope    = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesKnownBadInputsRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${var.project_name}-cf-waf"
+    sampled_requests_enabled   = true
+  }
+}
+
+output "waf_web_acl_arn" {
+  value = aws_wafv2_web_acl.cdn.arn
+}
